14b74eb7d7b2335843d8759418ff522d62dda358
MauroPello <mauro.pellonara@outlook.com>
2022-11-30 16:28:07 +0100

    Missing proper return codes and media support

---
+++ listener.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.get("/alexa-listener")
+def alexa_listener():
+    # checking if key is correct
+    key = request.args.get("key")
+    if key != "ciao35392ahgjai3":
+        return "Error, wrong key!"
+    
+    match request.args.get("action"):
+        case "shutdown":
+            # disconnect stereo bluetooth 
+            os.system("bluetoothctl disconnect 82:49:69:CC:D1:00")
+            # shutdown pc
+            os.system("qdbus org.kde.ksmserver /KSMServer logout 0 2 0")
+            return "success"
+        case "open-yt":
+            # query you tube
+            query = request.args.get("query")
+            print(query)
+            return "success"
+        case "open-ap":
+            # query amazon prime  
+            query = request.args.get("query")
+            print(query)
+            return "success"
+        case _:
+            return "Error, action not supported!"
