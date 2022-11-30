e7d9ecbd2bd26c2cefd30dc36de08a50675f90c2
MauroPello <mauro.pellonara@outlook.com>
2022-11-30 23:28:21 +0100

    Add status codes

--- listener.py
+++ listener.py
@@ -8,7 +8,7 @@ def alexa_listener():
     # checking if key is correct
     key = request.args.get("key")
     if key != "ciao35392ahgjai3":
-        return "Error, wrong key!"
+        return "Error, wrong key!", 401
     
     match request.args.get("action"):
         case "shutdown":
@@ -16,15 +16,15 @@ def alexa_listener():
             os.system("bluetoothctl disconnect 82:49:69:CC:D1:00")
             # shutdown pc
             os.system("qdbus org.kde.ksmserver /KSMServer logout 0 2 0")
-            return "success"
+            return "Success!", 200
         case "volume-up":
             os.system("pulseaudio-ctl up 10")
-            return "success"
+            return "Success!", 200
         case "volume-down":
             os.system("pulseaudio-ctl down 10")
-            return "success"
+            return "Success!", 200
         case "toggle":
-            os.system("playerctl play-pause -a")
-            return "success"
+            os.system("playerctl play-pause")
+            return "Success!", 200
         case _:
-            return "Error, action not supported!"
+            return "Error, action not supported!", 400
