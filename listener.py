4cde9b88ac8985ea9770f91229a715b235c7f2e6
MauroPello <mauro.pellonara@outlook.com>
2022-12-01 17:22:40 +0100

    Comment out all commands for alexa test

--- listener.py
+++ listener.py
@@ -13,18 +13,18 @@ def alexa_listener():
     match request.args.get("action"):
         case "shutdown":
             # disconnect stereo bluetooth 
-            os.system("bluetoothctl disconnect 82:49:69:CC:D1:00")
+            # os.system("bluetoothctl disconnect 82:49:69:CC:D1:00")
             # shutdown pc
-            os.system("qdbus org.kde.ksmserver /KSMServer logout 0 2 0")
+            # os.system("qdbus org.kde.ksmserver /KSMServer logout 0 2 0")
             return "Ok!", 200
         case "volume-up":
-            os.system("pulseaudio-ctl up 10")
+            # os.system("pulseaudio-ctl up 10")
             return "ok!", 200
         case "volume-down":
-            os.system("pulseaudio-ctl down 10")
+            #os.system("pulseaudio-ctl down 10")
             return "Ok!", 200
         case "toggle":
-            os.system("playerctl play-pause")
+            # os.system("playerctl play-pause")
             return "Ok!", 200
         case _:
             return "Non ho capito il comando!", 400
