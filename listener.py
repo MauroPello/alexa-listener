1d7ed6f82079782b5b33a436254781959acba1c4
MauroPello <mauro.pellonara@outlook.com>
2023-02-03 00:36:17 +0100

    Add YouTube, Netflix and Prime Video

--- listener.py
+++ listener.py
@@ -1,6 +1,12 @@
 import os
+from Google import Create_Service
 from flask import Flask, request
 
+CLIENT_SECRET_FILE = 'client_secret.json'
+API_NAME = 'youtube'
+API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis.com/auth/youtube']
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 app = Flask(__name__)
 
 @app.get("/alexa-listener")
@@ -12,19 +18,62 @@ def alexa_listener():
     
     match request.args.get("action"):
         case "shutdown":
+            os.system("pkill -INT chrome")
             # disconnect stereo bluetooth 
-            # os.system("bluetoothctl disconnect 82:49:69:CC:D1:00")
+            os.system("bluetoothctl disconnect 82:49:69:CC:D1:00")
             # shutdown pc
-            # os.system("qdbus org.kde.ksmserver /KSMServer logout 0 2 0")
+            os.system("qdbus org.kde.ksmserver /KSMServer logout 0 2 0")
             return "Ok!", 200
         case "volume-up":
-            # os.system("pulseaudio-ctl up 10")
-            return "ok!", 200
+            os.system("playerctl volume -a 0.15+")
+            return "Ok!", 200
         case "volume-down":
-            #os.system("pulseaudio-ctl down 10")
+            os.system("playerctl volume -a 0.15-")
             return "Ok!", 200
         case "toggle":
-            # os.system("playerctl play-pause")
+            os.system("playerctl play-pause")
+            return "Ok!", 200
+        case "video-yt":
+            query = request.args.get("query")
+            if not query:
+                return "Non hai fornito nessun video da cercare!", 401
+
+            # cerca e riproduci video
+            yt_request = service.search().list(
+                part="id",
+                maxResults=1,
+                q=query
+            )
+            yt_response = yt_request.execute()
+            link = "https://youtu.be/" + yt_response['items'][0]['id']['videoId']
+
+            os.system(f"google-chrome-stable \"{link}\" &")
+            os.system(f"sleep 3 && xdotool search --name \"{query}\" windowactivate mousemove 2500 1000 click --repeat 2 1")
+
+            return "Ok!", 200
+        case "video-ap":
+            query = request.args.get("query")
+            if not query:
+                return "Non hai fornito nessun video da cercare!", 401
+
+            # cerca e riproduci video
+            link = f"https://www.primevideo.com/search/ref=atv_nb_sr?phrase={query}&ie=UTF8"
+            
+            os.system(f"google-chrome-stable \"{link}\" &")
+            os.system(f"sleep 5 && xdotool search --name \"Prime Video\" windowactivate mousemove 2100 500 && sleep 1 && xdotool search --name \"Prime Video\" windowactivate mousemove 2100 650 click 1 && sleep 3 && xdotool search --name \"Prime Video\" windowactivate mousemove 4625 295 click 1")
+            
+            return "Ok!", 200
+        case "video-nf":
+            query = request.args.get("query")
+            if not query:
+                return "Non hai fornito nessun video da cercare!", 401
+
+            # cerca e riproduci video
+            link = "https://www.netflix.com/search?q=" + query
+
+            os.system(f"google-chrome-stable \"{link}\" &")
+            os.system(f"sleep 5 && xdotool search --name \"Netflix\" windowactivate mousemove 1500 600 && sleep 1 && xdotool search --name \"Netflix\" windowactivate mousemove 1380 880 click 1 && sleep 3 && xdotool search --name \"Netflix\" windowactivate click --repeat 2 1")
+            
             return "Ok!", 200
         case _:
             return "Non ho capito il comando!", 400
