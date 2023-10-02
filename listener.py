89576de890c667df798150827d36dacf3649bb3c
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-10-03 01:57:26 +0200

    Switch from using playerctl to sending media keys

--- listener.py
+++ listener.py
@@ -15,23 +15,24 @@ def alexa_listener():
     key = request.args.get("key")
     if key != "ciao35392ahgjai3":
         return "Hai sbagliato password!", 401
-    
+
     match request.args.get("action"):
         case "shutdown":
             os.system("pkill -INT chrome")
-            # disconnect stereo bluetooth 
+            # disconnect stereo bluetooth
             os.system("bluetoothctl disconnect 82:49:69:CC:D1:00")
             # shutdown pc
-            os.system("qdbus org.kde.ksmserver /KSMServer logout 0 2 0")
+            # os.system("qdbus org.kde.ksmserver /KSMServer logout 0 2 0")
+            os.system("shutdown now")
             return "Ok!", 200
         case "volume-up":
-            os.system("playerctl volume -a 0.15+")
+            os.system("xdotool key XF86AudioRaiseVolume")
             return "Ok!", 200
         case "volume-down":
-            os.system("playerctl volume -a 0.15-")
+            os.system("xdotool key XF86AudioLowerVolume")
             return "Ok!", 200
         case "toggle":
-            os.system("playerctl play-pause")
+            os.system("xdotool key XF86AudioPlay")
             return "Ok!", 200
         case "video-yt":
             query = request.args.get("query")
@@ -39,10 +40,10 @@ def alexa_listener():
                 return "Non hai fornito nessun video da cercare!", 401
             if "video dopo" in query.lower():
                 os.system("xdotool key --window $(xdotool search --name \"YouTube\") shift+n")
-                os.system("playerctl next")
+                os.system("xdotool key XF86AudioNext")
                 return "Ok!", 200
             if "video prima" in query.lower():
-                os.system("playerctl previous")
+                os.system("xdotool key XF86AudioPrev")
                 return "Ok!", 200
 
             # cerca e riproduci video
@@ -65,10 +66,10 @@ def alexa_listener():
 
             # cerca e riproduci video
             link = f"https://www.primevideo.com/search/ref=atv_nb_sr?phrase={query}&ie=UTF8"
-            
+
             os.system(f"google-chrome-stable \"{link}\" &")
             os.system(f"sleep 5 && xdotool search --name \"Prime Video\" windowactivate mousemove 2100 500 && sleep 1 && xdotool search --name \"Prime Video\" windowactivate mousemove 2100 650 click 1 && sleep 3 && xdotool search --name \"Prime Video\" windowactivate mousemove 4625 295 click 1")
-            
+
             return "Ok!", 200
         case "video-nf":
             query = request.args.get("query")
@@ -80,7 +81,7 @@ def alexa_listener():
 
             os.system(f"google-chrome-stable \"{link}\" &")
             os.system(f"sleep 5 && xdotool search --name \"Netflix\" windowactivate mousemove 1500 600 && sleep 1 && xdotool search --name \"Netflix\" windowactivate mousemove 1380 880 click 1 && sleep 3 && xdotool search --name \"Netflix\" windowactivate click --repeat 2 1")
-            
+
             return "Ok!", 200
         case _:
             return "Non ho capito il comando!", 400
